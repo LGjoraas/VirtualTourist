c75b6f1e690cddd003162d83de704f7b2bccc944
Ryan Gjoraas <ryan.gjoraas@gmail.com>
2018-07-23 11:13:24 -0600

    first commit

---
+++ VirtualTourist/Controller/PhotoAlbumView.swift
@@ -0,0 +1,10 @@
+//
+//  ImageCollectionView.swift
+//  VirtualTourist
+//
+//  Created by Lindsey Gjoraas on 7/23/18.
+//  Copyright © 2018 Developed by Gjoraas. All rights reserved.
+//
+
+import UIKit
+
---
+++ VirtualTourist/Controller/PhotoAlbumViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoAlbumViewCell.swift
+//  VirtualTourist
+//
+//  Created by Ryan Gjoraas on 7/23/18.
+//  Copyright © 2018 Developed by Gjoraas. All rights reserved.
+//
+
+import Foundation
