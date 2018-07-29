e9290bb4e21b98000663df8bff281b34714eb7ea
Ryan Gjoraas <ryan.gjoraas@gmail.com>
2018-07-29 09:21:39 -0600

    changed data model slightly and added class definition extensions for Pin and Photo

---
+++ VirtualTourist/Model/Photo+Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  Photo+Extensions.swift
+//  VirtualTourist
+//
+//  Created by Ryan Gjoraas on 7/29/18.
+//  Copyright © 2018 Developed by Gjoraas. All rights reserved.
+//
+
+
+import Foundation
+import CoreData
+
+extension Photo {
+    
+    static let name = "Photo"
+    
+    convenience init(imageUrl: String, forPin: Pin, context: NSManagedObjectContext) {
+        if let ent = NSEntityDescription.entity(forEntityName: Photo.name, in: context) {
+            self.init(entity: ent, insertInto: context)
+            self.imageURL = imageUrl
+            self.pin = forPin
+        } else {
+            fatalError("Unable to find Entity name!")
+        }
+    }
+}
---
+++ VirtualTourist/Model/Pin+Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  Pin+Extensions.swift
+//  VirtualTourist
+//
+//  Created by Ryan Gjoraas on 7/29/18.
+//  Copyright © 2018 Developed by Gjoraas. All rights reserved.
+//
+
+
+import Foundation
+import CoreData
+
+extension Pin {
+    
+    static let name = "Pin"
+    
+    convenience init(latitude: Double, longitude: Double, context: NSManagedObjectContext) {
+        if let ent = NSEntityDescription.entity(forEntityName: Pin.name, in: context) {
+            self.init(entity: ent, insertInto: context)
+            self.latitude = latitude
+            self.longitude = longitude
+        } else {
+            fatalError("Unable to find Entity name!")
+        }
+    }
+}
